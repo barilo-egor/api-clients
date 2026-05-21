c5d40bf740d70094035ef49bd6fe44c1d84b93f7
vdovin <V.Vdovin@isida.by>
2026-05-21 14:32:22 +0300

    fix interceptor

--- src/main/java/tgb/cryptoexchange/apiclients/controller/handler/GlobalGrpcExceptionHandler.java
+++ src/main/java/tgb/cryptoexchange/apiclients/controller/handler/GlobalGrpcExceptionHandler.java
@@ -82,15 +82,13 @@ public class GlobalGrpcExceptionHandler implements ServerInterceptor {
                 .setCode(code.getNumber())
                 .setMessage(message);
 
-        if (field != null && description != null) {
-            com.google.rpc.BadRequest badRequest = com.google.rpc.BadRequest.newBuilder()
-                    .addFieldViolations(com.google.rpc.BadRequest.FieldViolation.newBuilder()
-                            .setField(field)
-                            .setDescription(description)
-                            .build())
-                    .build();
-            statusBuilder.addDetails(com.google.protobuf.Any.pack(badRequest));
-        }
+        com.google.rpc.BadRequest badRequest = com.google.rpc.BadRequest.newBuilder()
+                .addFieldViolations(com.google.rpc.BadRequest.FieldViolation.newBuilder()
+                        .setField(field != null ? field : "")
+                        .setDescription(description != null ? description : "")
+                        .build())
+                .build();
+        statusBuilder.addDetails(com.google.protobuf.Any.pack(badRequest));
         return StatusProto.toStatusRuntimeException(statusBuilder.build());
     }
 
